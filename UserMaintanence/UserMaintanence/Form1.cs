b9f90cc13e67c2a7eaba1d884de24a269978b637
Balázs Molnár <balazs.molnar@stud.uni-corvinus.hu>
2020-09-23 22:04:19 +0200

    feladat6

--- UserMaintanence/UserMaintanence/Form1.cs
+++ UserMaintanence/UserMaintanence/Form1.cs
@@ -7,11 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UserMaintanence.Entities;
 
 namespace UserMaintanence
 {
     public partial class Form1 : Form
     {
+        BindingList<User> users = new BindingList<User>();
+
         public Form1()
         {
 
@@ -22,7 +25,22 @@ namespace UserMaintanence
             label1.Text = Resource1.LastName;
             label2.Text = Resource1.FirstName;
             button1.Text = Resource1.Ads;
+
+
+            listBox1.DataSource = users;
+            listBox1.ValueMember = "ID";
+            listBox1.DisplayMember = "FullName";
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var u = new User()
+            {
+                LastName = textBox1.Text,
+                FirstName = textBox2.Text
+            };
+
+            users.Add(u);
+        }
     }
 }
