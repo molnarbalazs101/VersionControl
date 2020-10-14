3f39e4c87bd94c84f7f8f7173d04ccaee4bbb866
Molnár Balázs <molnar.balazs101@gmail.com>
2020-10-14 23:01:18 +0200

    Webszolgáltatás hívása

--- hét6_MolnárBalázs/hét6_MolnárBalázs/Form1.cs
+++ hét6_MolnárBalázs/hét6_MolnárBalázs/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using hét6_MolnárBalázs.MnbServiceReference;
 
 namespace hét6_MolnárBalázs
 {
@@ -15,6 +16,29 @@ namespace hét6_MolnárBalázs
         public Form1()
         {
             InitializeComponent();
+
+            függvény();
+
+
         }
+
+        private void függvény()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+
+            var result = response.GetExchangeRatesResult;
+
+
+        }
+           
     }
 }
