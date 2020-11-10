22b704d4eac82eabf3c0710d6f02765b4fc10083
Molnár Balázs <molnar.balazs101@gmail.com>
2020-11-10 21:20:51 +0100

    Gyár absztrakció kezelése

---
+++ tasks_week08/tasks_week08/Abstractions/IToyFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tasks_week08.Abstractions
+{
+    public interface IToyFactory
+    {
+
+        Toy CreateNew();
+    }
+}
